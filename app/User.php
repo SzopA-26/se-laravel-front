cffda4376c0a2a364aacab8f1ca9b8d0e8900d64
SzopA-26 <patiphat.kh@ku.th>
2020-11-24 02:16:41 +0700

    dev report

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class User extends Authenticatable
 {
